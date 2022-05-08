4b3b2ae230d7a1a336d1f67104576980ea278985
Kei Tachikawa <aeres99@gmail.com>
2022-05-08 12:02:29 +0530

    new problems failed attempts

--- 750A/problem.c
+++ 750A/problem.c
@@ -2,10 +2,15 @@
 #include <math.h>
 int main() {
     int questions, total_time;
-    int time, s, n;
+    int time, s = 0, i, n = 1;
     scanf("%d %d", &questions, &total_time);
     time = 240 - total_time;
-    n = (sqrt((8 * time)/5 + 1) - 1)/ 2;
+    for (i = 1; (i < 10) && ((time - 5*n) > 0); i++) {
+        time = time - 5*i;
+        n = i;
+        printf("%d %d\n", time, 5*i);
+    }
+    //n = (sqrt((8 * time)/5 + 1) - 1)/ 2;
     printf("%d", n);
     return 0;
 }
